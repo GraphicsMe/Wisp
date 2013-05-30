26aa29478e55ea12185b1c55440e04f3dd842c49
Andy <worton.wu@gmail.com>
2013-05-30 23:24:15 +0800

    this microfacet is similar to rough plastic

--- src/bsdf/microfacet.cpp
+++ src/bsdf/microfacet.cpp
@@ -14,26 +14,22 @@ public:
         m_extIOR = paramSet.getFloat("extIOR", 1.000277f); // air
         m_kd = paramSet.getColor("kd", Color3f(0.5f));
         m_ks = 1.f - vectorMax(m_kd);
-        std::string strType = paramSet.getString("type", "phong");
-        m_distribution = new MicrofacetDistribution(strType);
+        m_distribution = new MicrofacetDistribution(paramSet.getString("type", "beckmann"));
+    }
+
+    inline Vector3f reflect(const Vector3f& wi, const Normal3f& n) const
+    {
+        return 2.f * glm::dot(wi, n) * n - wi;
     }
 
     Color3f sample_f(BSDFQueryRecord& bRec, const Point2f& sample) const
     {
-        bRec;
-        sample;
-        assert (0);
-        if (Frame::cosTheta(bRec.wo) <= 0)
-            return Color3f(0.0f);
-        return Color3f(1.0f);
+        float pdf;
+        return this->sample_f(bRec, pdf, sample);
     }
 
     Color3f sample_f(BSDFQueryRecord& bRec, float& pdf, const Point2f& sample) const
     {
-        pdf;
-        bRec;
-        sample;
-        assert (0);
         if (Frame::cosTheta(bRec.wo) <= 0)
             return Color3f(0.0f);
         return Color3f(1.0f);
